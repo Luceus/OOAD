dbb3bcdf9ab6b308cc78f573f5eb9121ee85418c
Luceus <44517668+Luceus@users.noreply.github.com>
2020-11-27 20:03:12 +0700

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/BaiHat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class BaiHat
+    {
+        public String TenBaiHat { get; set; }
+        public String TenCaSi { get; set; }
+        public String TenAlbum { get; set; }
+        public float DoDai { get; set; }
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Menu
+    {
+        public void Display()
+        {
+            Console.WriteLine("Danh sách bài hát");
+            
+            Console.WriteLine("Tìm kiếm bài hát");
+        }
+    }
+}
